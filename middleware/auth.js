86ebdbd40fc63b2b430e17ef33865f46086f1fa4
Cheseed Sharon Bangura <cheseedsharonbangura@Cheseeds-MacBook-Pro.local>
2025-08-03 03:38:35 +0000

    authorisation updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
-  const token = req.header("x-auth-token");
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1]; // Expects "Bearer TOKEN"
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
